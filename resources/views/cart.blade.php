aafc0e37f96ab0403e13c1b028a13300bc90ca8f
Bimo Prakoso <omib.ami@gmail.com>
2022-06-07 15:53:41 +0700

    Fix bug when cart is empty

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,8 +4,8 @@
   <div class="container">
     <div class="row">
       <div class="col col-md-8">
-        @if(count($errors) > 0)
-          @foreach($errors->all() as $error)
+        @if (count($errors) > 0)
+          @foreach ($errors->all() as $error)
             <div class="alert alert-warning">{{ $error }}</div>
           @endforeach
         @endif
@@ -26,118 +26,121 @@
           <div class="card-body">
             <table class="table table-stripped">
               <thead>
-              <tr>
-                <th>No</th>
-                <th>Produk</th>
-                <th>Harga</th>
-                <th>Qty</th>
-                <th>Subtotal</th>
-                <th></th>
-              </tr>
+                <tr>
+                  <th>No</th>
+                  <th>Produk</th>
+                  <th>Harga</th>
+                  <th>Qty</th>
+                  <th>Subtotal</th>
+                  <th></th>
+                </tr>
               </thead>
               <tbody>
-              @if(Auth::check())
-                @foreach($itemcart->detail as $detail)
-                  <tr>
-                    <td>
-                      {{ $no++ }}
-                    </td>
-                    <td>
-                      {{ $detail->product->name }}
-                    </td>
-                    <td>
-                      {{ number_format($detail->price, 2) }}
-                    </td>
-                    <td>
-                      <div class="btn-group" role="group">
-                        <form action="{{ route('cart.update',$detail->id) }}" method="post">
-                          @method('patch')
-                          @csrf()
-                          <input type="hidden" name="param" value="kurang">
-                          <button class="btn btn-primary btn-sm">
-                            -
-                          </button>
-                        </form>
-                        <button class="btn btn-outline-primary btn-sm" disabled="true">
-                          {{ number_format($detail->quantity, 2) }}
-                        </button>
-                        <form action="{{ route('cart.update',$detail->id) }}" method="post">
-                          @method('patch')
-                          @csrf()
-                          <input type="hidden" name="param" value="tambah">
-                          <button class="btn btn-primary btn-sm">
-                            +
-                          </button>
-                        </form>
-                      </div>
-                    </td>
-                    <td>
-                      {{ number_format($detail->subtotal, 2) }}
-                    </td>
-                    <td>
-                      <form action="{{ route('cart.destroy', $detail->id) }}" method="post" style="display:inline;">
-                        @csrf
-                        {{ method_field('delete') }}
-                        <button type="submit" class="btn btn-sm btn-danger mb-2">
-                          Hapus
-                        </button>
-                      </form>
-                    </td>
-                  </tr>
-                @endforeach
-              @else
-                @if(session('cart'))
-                  <?php $total = 0 ?>
-                  @foreach($cartsession as $cartse)
-                    <?php $total += $cartse['price'] * $cartse['quantity'] ?>
-                    <tr>
-                      <td>
-                        {{ $no++ }}
-                      </td>
-                      <td>
-                        {{ $cartse['name'] }}
-                      </td>
-                      <td>
-                        {{ number_format($cartse['price'], 2) }}
-                      </td>
-                      <td>
-                        <div class="btn-group" role="group">
-                          <form action="{{ route('cart.quantity',$cartse['id']) }}" method="post">
-                            @method('patch')
-                            @csrf()
-                            <input type="hidden" name="param" value="kurang">
-                            <button class="btn btn-primary btn-sm">
-                              -
+                @if (Auth::check())
+                  @if (isset($itemcart))
+                    @foreach ($itemcart->detail as $detail)
+                      <tr>
+                        <td>
+                          {{ $no++ }}
+                        </td>
+                        <td>
+                          {{ $detail->product->name }}
+                        </td>
+                        <td>
+                          {{ number_format($detail->price, 2) }}
+                        </td>
+                        <td>
+                          <div class="btn-group" role="group">
+                            <form action="{{ route('cart.update', $detail->id) }}" method="post">
+                              @method('patch')
+                              @csrf()
+                              <input type="hidden" name="param" value="kurang">
+                              <button class="btn btn-primary btn-sm">
+                                -
+                              </button>
+                            </form>
+                            <button class="btn btn-outline-primary btn-sm" disabled="true">
+                              {{ number_format($detail->quantity, 2) }}
+                            </button>
+                            <form action="{{ route('cart.update', $detail->id) }}" method="post">
+                              @method('patch')
+                              @csrf()
+                              <input type="hidden" name="param" value="tambah">
+                              <button class="btn btn-primary btn-sm">
+                                +
+                              </button>
+                            </form>
+                          </div>
+                        </td>
+                        <td>
+                          {{ number_format($detail->subtotal, 2) }}
+                        </td>
+                        <td>
+                          <form action="{{ route('cart.destroy', $detail->id) }}" method="post"
+                            style="display:inline;">
+                            @csrf
+                            {{ method_field('delete') }}
+                            <button type="submit" class="btn btn-sm btn-danger mb-2">
+                              Hapus
                             </button>
                           </form>
-                          <button class="btn btn-outline-primary btn-sm" disabled="true">
-                            {{ number_format($cartse['quantity'], 2) }}
-                          </button>
-                          <form action="{{ route('cart.quantity', $cartse['id']) }}" method="post">
-                            @method('patch')
+                        </td>
+                      </tr>
+                    @endforeach
+                  @endif
+                @else
+                  @if (session('cart'))
+                    <?php $total = 0; ?>
+                    @foreach ($cartsession as $cartse)
+                      <?php $total += $cartse['price'] * $cartse['quantity']; ?>
+                      <tr>
+                        <td>
+                          {{ $no++ }}
+                        </td>
+                        <td>
+                          {{ $cartse['name'] }}
+                        </td>
+                        <td>
+                          {{ number_format($cartse['price'], 2) }}
+                        </td>
+                        <td>
+                          <div class="btn-group" role="group">
+                            <form action="{{ route('cart.quantity', $cartse['id']) }}" method="post">
+                              @method('patch')
+                              @csrf()
+                              <input type="hidden" name="param" value="kurang">
+                              <button class="btn btn-primary btn-sm">
+                                -
+                              </button>
+                            </form>
+                            <button class="btn btn-outline-primary btn-sm" disabled="true">
+                              {{ number_format($cartse['quantity'], 2) }}
+                            </button>
+                            <form action="{{ route('cart.quantity', $cartse['id']) }}" method="post">
+                              @method('patch')
+                              @csrf()
+                              <input type="hidden" name="param" value="tambah">
+                              <button class="btn btn-primary btn-sm update-cart">
+                                +
+                              </button>
+                            </form>
+                          </div>
+                        </td>
+                        <td>
+                          {{ number_format($cartse['price'] * $cartse['quantity'], 2) }}
+                        </td>
+                        <td class="actions" data-th="">
+                          <form action="{{ route('cart.remove', $cartse['id']) }}" method="post">
+                            @method('delete')
                             @csrf()
-                            <input type="hidden" name="param" value="tambah">
-                            <button class="btn btn-primary btn-sm update-cart">
-                              +
+                            <button class="btn btn-sm btn-danger mb-2" data-id="{{ $cartse['id'] }}">Hapus
                             </button>
                           </form>
-                        </div>
-                      </td>
-                      <td>
-                        {{ number_format(($cartse['price'] * $cartse['quantity']), 2) }}
-                      </td>
-                      <td class="actions" data-th="">
-                        <form action="{{ route('cart.remove', $cartse['id']) }}" method="post">
-                          @method('delete')
-                          @csrf()
-                          <button class="btn btn-sm btn-danger mb-2" data-id="{{ $cartse['id'] }}">Hapus
-                          </button>
-                        </form>
-                      </td>
-                    </tr>
-                  @endforeach
+                        </td>
+                      </tr>
+                    @endforeach
+                  @endif
                 @endif
-              @endif
               </tbody>
             </table>
           </div>
@@ -149,33 +152,35 @@
             Ringkasan
           </div>
           <div class="card-body">
-            @if(Auth::check())
-              <table class="table">
-                <tr>
-                  <td>No Invoice</td>
-                  <td class="text-right">
-                    {{ $itemcart->no_invoice }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Subtotal</td>
-                  <td class="text-right">
-                    {{ number_format($itemcart->subtotal, 2) }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>Total</td>
-                  <td class="text-right">
-                    {{ number_format($itemcart->total, 2) }}
-                  </td>
-                </tr>
-              </table>
+            @if (Auth::check())
+              @if (isset($itemcart))
+                <table class="table">
+                  <tr>
+                    <td>No Invoice</td>
+                    <td class="text-right">
+                      {{ $itemcart->no_invoice }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Subtotal</td>
+                    <td class="text-right">
+                      {{ number_format($itemcart->subtotal, 2) }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Total</td>
+                    <td class="text-right">
+                      {{ number_format($itemcart->total, 2) }}
+                    </td>
+                  </tr>
+                </table>
+              @endif
             @else
               <table class="table">
                 <tr>
                   <td>Subtotal</td>
                   <td class="text-right">
-                    @if(session('cart'))
+                    @if (session('cart'))
                       {{ number_format($total, 2) }}
                     @endif
                   </td>
@@ -183,7 +188,7 @@
                 <tr>
                   <td>Total</td>
                   <td class="text-right">
-                    @if(session('cart'))
+                    @if (session('cart'))
                       {{ number_format($total, 2) }}
                     @endif
                   </td>
@@ -196,24 +201,28 @@
               <div class="col">
                 <form action="{{ route('checkout.add') }}" method="post">
                   @csrf
-                  @if(Auth::check())
-                    <input type="hidden" name="total" value="{{ $itemcart->total }}">
-                    <input type="hidden" name="id_cart" value="{{ $itemcart->id }}">
+                  @if (Auth::check())
+                    @if (isset($itemcart))
+                      <input type="hidden" name="total" value="{{ $itemcart->total }}">
+                      <input type="hidden" name="id_cart" value="{{ $itemcart->id }}">
+                    @endif
                   @elseif(session('cart'))
                     <input type="hidden" name="total" value="{{ number_format($total, 2) }}">
                   @else
                     <input type="hidden" name="total" value="0">
                   @endif
-                    <button class="btn btn-primary btn-block">Checkout</button>
+                  <button class="btn btn-primary btn-block">Checkout</button>
                 </form>
               </div>
               <div class="col">
-                @if(Auth::check())
-                  <form action="{{ route('cart.kosongkan', $itemcart->id) }}" method="post">
-                    @method('patch')
-                    @csrf()
-                    <button type="submit" class="btn btn-danger btn-block">Kosongkan</button>
-                  </form>
+                @if (Auth::check())
+                  @if (isset($itemcart))
+                    <form action="{{ route('cart.kosongkan', $itemcart->id) }}" method="post">
+                      @method('patch')
+                      @csrf()
+                      <button type="submit" class="btn btn-danger btn-block">Kosongkan</button>
+                    </form>
+                  @endif
                 @else
                   <form action="{{ route('cart.empty') }}" method="post">
                     @method('post')
